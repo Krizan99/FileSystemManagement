7f4204b9342ebdeee0f78c1d7af458fab6e27b99
Andrej Križan <andrej.krizan@htecgroup.com>
2022-09-11 15:00:50 +0200

    added .gitignore

--- Models/Message.cs
+++ Models/Message.cs
@@ -1,22 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace FileSystemManagement.Models
+﻿namespace FileSystemManagement.Models
 {
     public class Message
     {
         public int Id { get; private set; }
         public string Text { get; private set; }
         public bool IsDeleted { get; private set; }
-        public int SenderId { get; private set; }
-        public int ReceiverId { get; private set; }
 
+        public int SenderId { get; private set; }
         public User Sender { get; private set; }
 
+        public int ReceiverId { get; private set; }
         public User Receiver { get; private set; }
-    
+
+        public Message(string text, int senderId, int receiverId)
+        {
+            Text = text;
+            SenderId = senderId;
+            ReceiverId = receiverId;
+        }
+
+        private Message() { }
     }
 }
